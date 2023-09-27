aac3f9095383da64e76c8c4d1bf892b4df069adf
xjaso <xjasonwei@gmail.com>
2023-09-26 22:39:25 -0400

    Added input for name and quantity of a food Printed receipt to show amount, price, item, table and cashier name.

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -6,31 +6,51 @@ public class TipCalculator {
         System.out.println("Welcome to the tip calculator!");
         System.out.print("Enter table name: ");
         String tableName = scan.nextLine();
+        System.out.print("Cashier name: ");
+        String cashierName = scan.nextLine();
         System.out.print("Enter the number of people: ");
         int numPeople = scan.nextInt();
-
         double totalPrice = 0.00;
+        String receipt = "";
 
         while (true) {
-            System.out.print("Enter a cost in dollars and cents (-1 to end): ");
-            float foodCost = scan.nextFloat();
-            totalPrice += foodCost;
-            if (foodCost == -1) {
-                totalPrice += 1;
+            System.out.print("\nEnter the name of the food (-1 to end): ");
+            scan.nextLine();
+            String foodName = scan.nextLine();
+            if (foodName.equals("-1")) {
                 break;
             }
+            System.out.print("Enter the quantity: ");
+            int foodQuantity = scan.nextInt();
+            scan.nextLine();
+            System.out.print("Enter a cost in dollars and cents: $");
+            double foodCost = scan.nextDouble();
+            totalPrice += foodQuantity * foodCost;
+            receipt += foodQuantity + "x\t\t\t$" + foodCost + "\t\t\t"+ foodName + "\n";
         }
-        System.out.println("Enter tip percent: ");
-        double tipPercent = scan.nextDouble();
-        System.out.println("Total bill before tip: $" + totalPrice);
-        double averagePrice = totalPrice/numPeople;
-        System.out.println("Average bill per person: " + String.format("%.2f", averagePrice));
-        System.out.println("Tip Percent: " + tipPercent + "%");
-        double totalTip = (double) tipPercent / 100 * totalPrice;
-        System.out.println("Total Tip: " + String.format("%.2f", totalTip));
-        System.out.println("Average tip per person: $" + String.format("%.2f", totalPrice/numPeople));
-        System.out.println("Total bill with tip: $" + String.format("%.2f", totalPrice + totalTip));
+        System.out.print("\nEnter tip percent: ");
 
+        double tipPercent = scan.nextDouble();
+        double averagePrice = totalPrice/numPeople;
+        double totalTip = (double) tipPercent / 100 * totalPrice;
+        double tipPerPerson = totalTip/numPeople;
+        double totalBill = totalPrice + totalTip;
+
+        System.out.println("Total bill before tip: $" + String.format("%.2f", totalPrice));
+        System.out.println("Bill before tip per person: $" + String.format("%.2f", averagePrice));
+        System.out.println("Tip Percent: " + tipPercent + "%");
+        System.out.println("Total Tip: $" + String.format("%.2f", totalTip));
+        System.out.println("Tip per person: $" + String.format("%.2f", tipPerPerson));
+        System.out.println("Total bill: $" + String.format("%.2f", totalBill));
+        System.out.println("Total bill per person: $" + totalBill / numPeople);
+
+        System.out.println("\n------------------------------------");
+        System.out.println("Receipt");
+        System.out.println("Amount\t\tPrice\t\tItem");
+        System.out.println(receipt);
+        System.out.println("Table: " + tableName);
+        System.out.println("Cashier: " + cashierName);
+        System.out.println("------------------------------------");
         scan.close();
     }
 }
